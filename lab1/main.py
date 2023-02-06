b6658d4f8ea85c97aab5283e390cc7837bad4c51
VladyslavKatsap <katsap.vladyslav.clg@chnu.edu.ua>
2023-02-06 14:54:57 +0200

    перше завдання повністю виконано

--- lab1/main.py
+++ lab1/main.py
@@ -4,3 +4,14 @@ matrix1 = m.read_matrix_from_file("matr.txt")
 
 m.print_matrix(matrix1)
 
+# vector = []
+# for i in range(5):
+#    value = int(input("Enter value for element " + str(i) + ": "))
+#    vector.append(value)
+
+# print(m.matrix_vector_multiply(matrix1, vector))
+
+a = int(input("Enter value for А = "))
+row = int(input("Enter row = "))
+print(m.matrix_row_multiply(matrix1, row, a))
+m.print_matrix(matrix1)
